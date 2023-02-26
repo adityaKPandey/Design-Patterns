df6fa4452c83769dedcfa9b070641cf8162dfc67
aditya <adityapandey1207@gmail.com>
2023-02-27 02:03:39 +0530

    SOLID principles : fixed compilation error

--- Common-Design-Patterns/src/com/solid/principles/openclosed/violation/InvoiceDao.java
+++ Common-Design-Patterns/src/com/solid/principles/openclosed/violation/InvoiceDao.java
@@ -5,6 +5,8 @@ package com.solid.principles.openclosed.violation;
   This violates Open-Closed principle which says a class should be closed for modification and open for extension.
 */
 
+import com.solid.principles.openclosed.Invoice;
+
 public class InvoiceDao {
 
   private Invoice invoice;

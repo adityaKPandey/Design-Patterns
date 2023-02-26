e44804cc136f66bb99b849ceea649c38576eec3c
aditya <adityapandey1207@gmail.com>
2023-02-27 02:06:31 +0530

    SOLID principles : added more description for Liskov's Substitution principle

--- Common-Design-Patterns/src/com/solid/principles/liskovsubstitution/violation/Bike.java
+++ Common-Design-Patterns/src/com/solid/principles/liskovsubstitution/violation/Bike.java
@@ -1,5 +1,6 @@
 package com.solid.principles.liskovsubstitution.violation;
 /*
+   Liskov's Substitution :
    If class B is subtype of class A , then we should be able to replace object of A with object of B
    without breaking the behavior of the program.
 
@@ -8,8 +9,8 @@ package com.solid.principles.liskovsubstitution.violation;
    Eg. Here we have a Bike interface with 2 methods to turn on engine and to accelerate. Now MotorBike class
    can implement this interface and provide implementations of both these methods. But a bicycle  doesn't have
    an engine . So when Bicycle class implements Bike interface , it throws exception from turnOnEngine method-
-   this is changing the behavior of parent class Bike : narrowing down the capability of bike . This violates
-   Liskov Substitution principle.
+   this is changing the behavior of parent class Bike : narrowing down the capability of bike . It will cause any 3rd party
+   program passing Bicycle in place of MotoBike as parameter for Bike . This violates Liskov Substitution principle.
 
  */
 public interface Bike {
